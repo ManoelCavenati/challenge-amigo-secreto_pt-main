ddc716e7942e41fc54679bf910a022c78e86fac1
cavenati <140075906+ManoelCavenati@users.noreply.github.com>
2025-02-01 16:25:02 -0300

    Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  const input = document.querySelector("amigo");
+  const nome = input.value;
+
+  // Validação: se o campo estiver vazio, exibe um alerta pedindo para inserir um amigo.
+  if (nome === "") {
+    alert("Por favor, insira o nome do amigo");
+    return;
+  }
+
+  amigos.push(nome);
+
+  input.value = "";
+}
